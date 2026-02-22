5cb2cfb4656dfd8d912b5a552a9dcb2518f846d7
Masaya Morita <47968855+Masaya0606@users.noreply.github.com>
2026-02-22 16:32:06 +0900

    Update King.sh

--- King.sh
+++ King.sh
@@ -1,1 +1,11 @@
-singularity exec /usr/local/biotools/k/king\:2.2.7--hd03093a_0 king -b  PoritesPcyl2503_4_geno01_maf005_hwe_ed.bed --kinship --degree 3
+#!/bin/sh
+#SBATCH --job-name=BWA
+#SBATCH --output=BWA.out
+#SBATCH --error=BWA.err
+#SBATCH --time=40:00:00
+#SBATCH --mem=10G
+#SBATCH --ntasks=8
+#SBATCH --partition=rome
+
+# please change the number after --degree
+singularity exec /usr/local/biotools/k/king\:2.2.7--hd03093a_0 king -b  PoritesPcyl2503_4_geno01_maf005_hwe_ed.bed --kinship --degree 1
